665c8d5ed8df78640e862dcf7fbde729f407a4a5
VetlixDev <80253872+VetlixDev@users.noreply.github.com>
2021-09-30 22:51:44 +0100

    Added interactions for buttons

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,30 @@
 const client = require("../index");
+const simplydjs = require("simply-djs");
+const {
+    MessageActionRow,
+    MessageButton,
+    MessageEmbed
+} = require('discord.js');
 
 client.on("interactionCreate", async (interaction) => {
+    await interaction.deferUpdate();
+    //Simple Djs
+    simplydjs.clickBtn(interaction, {
+      embedDesc: 'This Is Description',
+      embedColor: '#fffff', // default: #075FFF
+      closeColor: '', //default: blurple
+      closeEmoji: '😃', // default: 🔒
+      
+      delColor: '', // default: grey
+      delEmoji: '', // default: ❌
+      openColor: '' , // default: green
+      openEmoji: '', // default: 🔓
+      timeout: false, // default: true | Needs to be boolean (true/false)
+    
+      categoryID: '881801380841541632',
+      role: '859824835802562590' // Role which sees the ticket channel (like Support Role)
+      })
+    
     // Slash Command Handling
     if (interaction.isCommand()) {
         await interaction.deferReply({ ephemeral: false }).catch(() => {});
@@ -29,5 +53,93 @@ client.on("interactionCreate", async (interaction) => {
         await interaction.deferReply({ ephemeral: false });
         const command = client.slashCommands.get(interaction.commandName);
         if (command) command.run(client, interaction);
+    }
+    
+    // Ticket System Python
+    if (interaction.isButton()) {
+        if (interaction.customId === 'python') {
+
+            const thread = await interaction.channel.threads.create({
+                name: `${interaction.user.tag}`,
+                autoArchiveDuration: 1440, 
+            });
+            await thread.setLocked(true)
+            const embed = new MessageEmbed()
+                .setTitle('Vetlix\'s Treehouse Ticket System')
+                .setDescription('Hello! Thank you for making a ticket! A Code Helper will be with you shortly. In the mean time Please post your code and error in code blocks so its easier for us to help you.')
+                .setColor('#bfff00')
+                .setTimestamp()
+                .setFooter('Vetlix-Utilities Ticket System')
+                .setAuthor(interaction.guild.name, interaction.guild.iconURL({
+                    dynamic: true
+                }));
+
+            const del = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                    .setCustomId('del')
+                    .setLabel('🔒 Close Ticket!')
+                    .setStyle('DANGER'),
+                );
+            interaction.user.send('You\'r Help Ticket Has been made!');
+            thread.send({
+                content: `<@${interaction.user.id}> <@&893118951058440192> <@&834767562939301951>`,
+                embeds: [embed],
+                components: [del]
+            }).then(interaction.followUp({
+                content: 'I have successfully Made your Ticket!',
+                ephemeral: true
+            }))
+            setTimeout(() => {
+                interaction.channel.bulkDelete(1)
+            }, 5000)
+        } else if (interaction.customId === 'del') {
+            const thread = interaction.channel
+            thread.delete();
+        }
+        
+       // Ticket System
+        if (interaction.isButton()) {
+           if (interaction.customId === 'javascript') {
+
+            const thread = await interaction.channel.threads.create({
+                name: `${interaction.user.tag}`,
+                autoArchiveDuration: 1440, 
+            });
+            await thread.setLocked(true)
+            const embed = new MessageEmbed()
+                .setTitle('Vetlix\'s Treehouse Ticket System')
+                .setDescription('Hello! Thank you for making a ticket! A Code Helper will be with you shortly. In the mean time Please post your code and error in code blocks so its easier for us to help you.')
+                .setColor('#bfff00')
+                .setTimestamp()
+                .setFooter('Vetlix-Utilities Ticket System')
+                .setAuthor(interaction.guild.name, interaction.guild.iconURL({
+                    dynamic: true
+                }));
+
+            const del = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                    .setCustomId('del')
+                    .setLabel('🔒 Close Ticket!')
+                    .setStyle('DANGER'),
+                );
+            interaction.user.send('You\'r Help Ticket Has been made!');
+            thread.send({
+                content: `<@${interaction.user.id}> <@&893118951058440192> <@&834767562939301951>`,
+                embeds: [embed],
+                components: [del]
+            }).then(interaction.followUp({
+                content: 'I have successfully Made your Ticket!',
+                ephemeral: true
+            }))
+            setTimeout(() => {
+                interaction.channel.bulkDelete(1)
+            }, 5000)
+        } else if (interaction.customId === 'del') {
+            const thread = interaction.channel
+            thread.delete();
+        }
+    }
     }
 });
